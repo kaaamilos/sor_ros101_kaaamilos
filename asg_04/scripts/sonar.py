dc5a8f80b67cf8f6dca1a73fd0c22da071e8e938
kaaamilos <kaaamilos@gmail.com>
2022-07-13 21:48:35 +0200

    feat: add left and right scanner

--- asg_04/scripts/sonar.py
+++ asg_04/scripts/sonar.py
@@ -2,9 +2,10 @@
 import rospy
 from sensor_msgs.msg import LaserScan, Range
 
-pub_topic = '/range'
-sub_topic = '/scan'
-pub = rospy.Publisher(pub_topic, Range, queue_size=10)
+pub_topic = ['/range_center', '/range_left', '/range_right']
+sub_topic = ['/scan_center', '/scan_left', '/scan_right']
+frame_ids = ['center_laser_link', 'left_laser_link', 'right_laser_link']
+pub = []
 
 def callback(data):
     rospy.loginfo(rospy.get_caller_id() + 'I heard %s', data.ranges[0])
@@ -16,9 +17,15 @@ def callback(data):
     range_msg.max_range = 30.0
     range_msg.min_range = 0.1
     range_msg.range = sum(data.ranges)/len(data.ranges)
-    pub.publish(range_msg)
+
+    sonar_number = frame_ids.index(str(range_msg.header.frame_id))
+    pub[sonar_number].publish(range_msg)
 
 rospy.init_node('sonar_node', anonymous=True)
-rospy.Subscriber(sub_topic, LaserScan, callback)
+for topic in sub_topic:
+    rospy.Subscriber(topic, LaserScan, callback)
+
+for topic in pub_topic:
+    pub.append(rospy.Publisher(topic, Range, queue_size=10))
 
 rospy.spin()
